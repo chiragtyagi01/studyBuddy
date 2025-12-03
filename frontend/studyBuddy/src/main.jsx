4d3f21846ea21d33cbe48f1132b69d0803052730
Chirag Tyagi <chiragtyagi2812004@gmail.com>
2025-12-03 23:44:07 +0530

    feat: Implement Redux slices for authentication, cart, course management, and profile; integrate Redux Toolkit and react-hot-toast

--- frontend/studyBuddy/src/main.jsx
+++ frontend/studyBuddy/src/main.jsx
@@ -1,22 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import { Provider } from 'react-redux'
+import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 
-const store=configureStore({
-  reducer:rootReducer,
-})
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
-    
-  </StrictMode>,
-)
+import rootReducer from "./reducer";
+
+const store = configureStore({
+  reducer: rootReducer,
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+        <Toaster />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
